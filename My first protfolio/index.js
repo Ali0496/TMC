f9de4adea232feea399d6a32d276bc2357be8ce6
Orange-P <orange-p@Orange-Ps-iMac.local>
2024-09-07 15:54:17 +0400

    adding burger button

---
+++ My first protfolio/index.js
@@ -0,0 +1,14 @@
+const burgerBtn = document.getElementById("burger");
+const closeBtn = document.getElementById("close-svg");
+const nav_items = document.querySelector(".nav-items");
+
+burgerBtn.addEventListener("click", () => {
+    nav_items.classList.toggle("open");
+    burgerBtn.style.display = "none";
+    closeBtn.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+    nav_items.classList.toggle("open");
+    burgerBtn.style.display = "block";
+    closeBtn.style.display = "none";
+})
